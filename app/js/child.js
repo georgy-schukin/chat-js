b4c539d68a20db1906908323c35347ae5934b777
Georgy Schukin <georgy.schukin@gmail.com>
2016-07-04 13:04:06 +0700

    Add sources

---
+++ app/js/child.js
@@ -0,0 +1,8 @@
+process.on("message", function(data) {
+	console.log("Child got message: " + data);
+	var result = data;
+	if (typeof data === "string" || data instanceof string) {
+		result = data.toUpperCase();
+	}
+	process.send(result);
+});
---
+++ app/server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var cp = require("child_process");
+
+app.use(express.static(__dirname));
+
+app.get('/', function(request, response) {
+	response.sendFile("index.html", {root: __dirname});
+});
+
+function computeInChild(data) {  
+  var child = cp.fork(__dirname + "/js/child.js");
+  child.on("message", function(msg) {    
+    io.emit("chat message", msg);
+    child.disconnect();
+  });
+  child.send(data);
+}
+
+io.on('connection', function(socket) {
+	console.log('a user connected');	
+	socket.on('disconnect', function() {
+    	console.log('user disconnected');
+  	});
+	socket.on('chat message', function(msg) {
+		if (msg.length > 0) {
+  		computeInChild(msg);
+  	}
+	});  
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
